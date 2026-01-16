1bc47b2de53278edde4ba6b2d8d164b0ffa87c7b
md anas <anas2542wy@gmail.com>
2026-01-16 20:11:23 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+
+import Navbar from './allComponents/Navbar'
+import './App.css'
+import Hero from './allComponents/Hero'
+import Banner from './allComponents/Banner'
+import Features from './allComponents/Features'
+import AllColors from './allComponents/AllColors'
+import ProductSpecs from './allComponents/ProductSpecs'
+import OrderForm from './allComponents/OrderForm'
+function App() {
+
+
+  return (
+    <>
+      <Navbar/>
+      <Hero/>
+      <Banner/>
+      <Features/>
+      <AllColors/>
+      <ProductSpecs/>
+      <OrderForm/>
+    </>
+  )
+}
+
+export default App;
